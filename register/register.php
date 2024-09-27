2c4535896a97abb2976d136e03822e9765c9b0dc
damir <delewev.damir@mail.ru>
2024-09-27 12:13:03 +0300

    register.php

---
+++ register/register.php
@@ -0,0 +1,21 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $firstName = htmlspecialchars(trim($_POST['first_name']));
+    $lastName = htmlspecialchars(trim($_POST['last_name']));
+    $email = htmlspecialchars(trim($_POST['email']));
+    $password = htmlspecialchars(trim($_POST['password']));
+    $confirmPassword = htmlspecialchars(trim($_POST['confirm_password']));
+
+    // Проверка на совпадение паролей
+    if ($password !== $confirmPassword) {
+        echo "Пароли не совпадают.";
+        exit;
+    }else{
+        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+    }
+
+    echo "Регистрация прошла успешно. Добро пожаловать, $firstName, $hashedPassword!";
+} else {
+    echo "Некорректный запрос.";
+}
